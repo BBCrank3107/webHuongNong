fa564de977f36a7c4dace57483509b778dbdfc60
Bình Nguyễn <134196134+BBCrank3107@users.noreply.github.com>
2023-06-03 00:27:57 +0700

    Upload files

---
+++ js/content.js
@@ -0,0 +1,331 @@
+// -------------Cây Cà phê
+const showModalButton1 = document.getElementById("showModalButton1");
+const closeModalButton1 = document.getElementById("closeModalButton1");
+const modal1 = document.getElementById("modal1");
+
+const showModalButton2 = document.getElementById("showModalButton2");
+const closeModalButton2 = document.getElementById("closeModalButton2");
+const modal2 = document.getElementById("modal2");
+
+const showModalButton3 = document.getElementById("showModalButton3");
+const closeModalButton3 = document.getElementById("closeModalButton3");
+const modal3 = document.getElementById("modal3");
+
+const showModalButton4 = document.getElementById("showModalButton4");
+const closeModalButton4 = document.getElementById("closeModalButton4");
+const modal4 = document.getElementById("modal4");
+
+// -------------Cây Cao su
+const showModalButton5 = document.getElementById("showModalButton5");
+const closeModalButton5 = document.getElementById("closeModalButton5");
+const modal5 = document.getElementById("modal5");
+
+const showModalButton6 = document.getElementById("showModalButton6");
+const closeModalButton6 = document.getElementById("closeModalButton6");
+const modal6 = document.getElementById("modal6");
+
+const showModalButton7 = document.getElementById("showModalButton7");
+const closeModalButton7 = document.getElementById("closeModalButton7");
+const modal7 = document.getElementById("modal7");
+
+const showModalButton8 = document.getElementById("showModalButton8");
+const closeModalButton8 = document.getElementById("closeModalButton8");
+const modal8 = document.getElementById("modal8");
+
+// -------------Cây điều
+const showModalButton9 = document.getElementById("showModalButton9");
+const closeModalButton9 = document.getElementById("closeModalButton9");
+const modal9 = document.getElementById("modal9");
+
+const showModalButton10 = document.getElementById("showModalButton10");
+const closeModalButton10 = document.getElementById("closeModalButton10");
+const modal10 = document.getElementById("modal10");
+
+const showModalButton11 = document.getElementById("showModalButton11");
+const closeModalButton11 = document.getElementById("closeModalButton11");
+const modal11 = document.getElementById("modal11");
+
+const showModalButton12 = document.getElementById("showModalButton12");
+const closeModalButton12 = document.getElementById("closeModalButton12");
+const modal12 = document.getElementById("modal12");
+
+// -------------Cây mía
+const showModalButton13 = document.getElementById("showModalButton13");
+const closeModalButton13 = document.getElementById("closeModalButton13");
+const modal13 = document.getElementById("modal13");
+
+const showModalButton14 = document.getElementById("showModalButton14");
+const closeModalButton14 = document.getElementById("closeModalButton14");
+const modal14 = document.getElementById("modal14");
+
+const showModalButton15 = document.getElementById("showModalButton15");
+const closeModalButton15 = document.getElementById("closeModalButton15");
+const modal15 = document.getElementById("modal15");
+
+const showModalButton16 = document.getElementById("showModalButton16");
+const closeModalButton16 = document.getElementById("closeModalButton16");
+const modal16 = document.getElementById("modal16");
+
+// -------------Cây thông
+const showModalButton17 = document.getElementById("showModalButton17");
+const closeModalButton17 = document.getElementById("closeModalButton17");
+const modal17 = document.getElementById("modal17");
+
+const showModalButton18 = document.getElementById("showModalButton18");
+const closeModalButton18 = document.getElementById("closeModalButton18");
+const modal18 = document.getElementById("modal18");
+
+const showModalButton19 = document.getElementById("showModalButton19");
+const closeModalButton19 = document.getElementById("closeModalButton19");
+const modal19 = document.getElementById("modal19");
+
+const showModalButton20 = document.getElementById("showModalButton20");
+const closeModalButton20 = document.getElementById("closeModalButton20");
+const modal20 = document.getElementById("modal20");
+
+// -------------Cây tiêu
+const showModalButton21 = document.getElementById("showModalButton21");
+const closeModalButton21 = document.getElementById("closeModalButton21");
+const modal21 = document.getElementById("modal21");
+
+const showModalButton22 = document.getElementById("showModalButton22");
+const closeModalButton22 = document.getElementById("closeModalButton22");
+const modal22 = document.getElementById("modal22");
+
+const showModalButton23 = document.getElementById("showModalButton23");
+const closeModalButton23 = document.getElementById("closeModalButton23");
+const modal23 = document.getElementById("modal23");
+
+const showModalButton24 = document.getElementById("showModalButton24");
+const closeModalButton24 = document.getElementById("closeModalButton24");
+const modal24 = document.getElementById("modal24");
+
+
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+const mainContent = document.getElementById("mainContent");
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+
+
+// -------------Cây cà phê
+showModalButton1.addEventListener("click", function () {
+    modal1.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton1.addEventListener("click", function () {
+    modal1.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton2.addEventListener("click", function () {
+    modal2.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton2.addEventListener("click", function () {
+    modal2.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton3.addEventListener("click", function () {
+    modal3.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton3.addEventListener("click", function () {
+    modal3.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton4.addEventListener("click", function () {
+    modal4.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton4.addEventListener("click", function () {
+    modal4.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+// -------------Cây cao su
+showModalButton5.addEventListener("click", function () {
+    modal5.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton5.addEventListener("click", function () {
+    modal5.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton6.addEventListener("click", function () {
+    modal6.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton6.addEventListener("click", function () {
+    modal6.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton7.addEventListener("click", function () {
+    modal7.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton7.addEventListener("click", function () {
+    modal7.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton8.addEventListener("click", function () {
+    modal8.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton8.addEventListener("click", function () {
+    modal8.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+// -------------Cây điều
+showModalButton9.addEventListener("click", function () {
+    modal9.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton9.addEventListener("click", function () {
+    modal9.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton10.addEventListener("click", function () {
+    modal10.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton10.addEventListener("click", function () {
+    modal10.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton11.addEventListener("click", function () {
+    modal11.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton11.addEventListener("click", function () {
+    modal11.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton12.addEventListener("click", function () {
+    modal12.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton12.addEventListener("click", function () {
+    modal12.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+// -------------Cây mía
+showModalButton13.addEventListener("click", function () {
+    modal13.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton13.addEventListener("click", function () {
+    modal13.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton14.addEventListener("click", function () {
+    modal14.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton14.addEventListener("click", function () {
+    modal14.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton15.addEventListener("click", function () {
+    modal15.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton15.addEventListener("click", function () {
+    modal15.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton16.addEventListener("click", function () {
+    modal16.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton16.addEventListener("click", function () {
+    modal16.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+// -------------Cây thông
+showModalButton17.addEventListener("click", function () {
+    modal17.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton17.addEventListener("click", function () {
+    modal17.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton18.addEventListener("click", function () {
+    modal18.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton18.addEventListener("click", function () {
+    modal18.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton19.addEventListener("click", function () {
+    modal19.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton19.addEventListener("click", function () {
+    modal19.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton20.addEventListener("click", function () {
+    modal20.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton20.addEventListener("click", function () {
+    modal20.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+// -------------Cây tiêu
+showModalButton21.addEventListener("click", function () {
+    modal21.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton21.addEventListener("click", function () {
+    modal21.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton22.addEventListener("click", function () {
+    modal22.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton22.addEventListener("click", function () {
+    modal22.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton23.addEventListener("click", function () {
+    modal23.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton23.addEventListener("click", function () {
+    modal23.style.display = "none";
+    containerMid.style.opacity = "1";
+});
+
+showModalButton24.addEventListener("click", function () {
+    modal24.style.display = "block";
+    containerMid.style.opacity = "0.5";
+});
+closeModalButton24.addEventListener("click", function () {
+    modal24.style.display = "none";
+    containerMid.style.opacity = "1";
+});
